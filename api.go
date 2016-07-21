07a85306c18204e05bb6163c4ca219afcf721b1c
Elis Axelsson <elis.axelsson@schibsted.se>
2016-07-21 15:26:50 +0200

    Added code for API

---
+++ api.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net"
+	"net/http"
+)
+
+func main() {
+	router := NewRouter()
+
+	log.Fatal(http.ListenAndServe(":8123", router))
+}
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", RootHandler)
+
+	return router
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	var ip string
+	addr := r.RemoteAddr
+
+	if proxy := r.Header.Get("x-forwarded-for"); proxy != "" {
+		ip = proxy
+	} else {
+		ip, _, _ = net.SplitHostPort(addr)
+	}
+
+	w.Write([]byte(ip))
+	w.Write([]byte("\n"))
+}
