d1d3ecbb88d7d4070d60448c242512ac33c734e5
Elis Hirwing <elis@hirwing.se>
2024-03-10 14:09:43 +0100

    fix: Format the nix file

--- flake.nix
+++ flake.nix
@@ -6,45 +6,58 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, nixpkgs, ... }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    packages = flake-utils.lib.flattenTree {
-      default = pkgs.buildGoModule (let
-        version = "1.0.${nixpkgs.lib.substring 0 8 self.lastModifiedDate}.${self.shortRev or "dirty"}";
-      in {
-        pname = "ip-failar-nu";
-        inherit version;
-
-        src = ./.;
-
-        vendorSha256 = "sha256-D2/PVtoJGljsfHVNX4Q+ASu5R3PnRakApEn1rZK4lgM=";
-      });
-    };
-
-    # Set up flake module
-    nixosModules.default = { options, config, lib, pkgs, ... }: let
-      cfg = config.services.ip-failar-nu;
-      pkg = self.packages.${system}.default;
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
     in {
-      # Set up module options
-      options.services.ip-failar-nu.enable = lib.mkEnableOption
-        "Service that responds over http with the connecting clients IP";
+      packages = flake-utils.lib.flattenTree {
+        default = pkgs.buildGoModule (let
+          version = "1.0.${nixpkgs.lib.substring 0 8 self.lastModifiedDate}.${self.shortRev or "dirty"}";
+        in {
+          pname = "ip-failar-nu";
+          inherit version;
 
-      # Set up module implementation
-      config = lib.mkIf cfg.enable {
-        systemd.services.ip-failar-nu = {
-          description = "ip-failar-nu";
-          after = [ "network.target" ];
-          wantedBy = [ "multi-user.target" ];
-          serviceConfig = {
-            Type = "simple";
-            User = "nobody";
-            ExecStart = "${pkg}/bin/ip.failar.nu";
-            Restart = "always";
+          src = ./.;
+
+          vendorSha256 = "sha256-D2/PVtoJGljsfHVNX4Q+ASu5R3PnRakApEn1rZK4lgM=";
+        });
+      };
+
+      # Set up flake module
+      nixosModules.default = {
+        options,
+        config,
+        lib,
+        pkgs,
+        ...
+      }: let
+        cfg = config.services.ip-failar-nu;
+        pkg = self.packages.${system}.default;
+      in {
+        # Set up module options
+        options.services.ip-failar-nu.enable =
+          lib.mkEnableOption
+          "Service that responds over http with the connecting clients IP";
+
+        # Set up module implementation
+        config = lib.mkIf cfg.enable {
+          systemd.services.ip-failar-nu = {
+            description = "ip-failar-nu";
+            after = ["network.target"];
+            wantedBy = ["multi-user.target"];
+            serviceConfig = {
+              Type = "simple";
+              User = "nobody";
+              ExecStart = "${pkg}/bin/ip.failar.nu";
+              Restart = "always";
+            };
           };
         };
       };
-    };
-  });
+    });
 }
